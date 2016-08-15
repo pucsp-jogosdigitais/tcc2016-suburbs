f0533144f411a8cad51a64c10a79bb05cc7fb4b1
Mariana Lopes <homurinhas@gmail.com>
2016-08-15 11:22:27 -0300

    versão menu Mario

---
+++ Assets/Scripts/MenuConfiguracoes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuConfiguracoes : MonoBehaviour
+{
+    public GameObject Principal;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            AtivarMenuPrincipal();
+    }
+
+    public void AtivarMenuPrincipal()
+    {
+        Principal.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public GameObject Configuracoes;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void AtivarMenuConfiguracoes()
+    {
+        Configuracoes.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
