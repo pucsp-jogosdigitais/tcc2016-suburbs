2c274a34c193a90fd0321e0c6caf89ded022e9e0
Mariana Lopes <homurinhas@gmail.com>
2016-08-15 12:52:18 -0300

    Menu voltar pro menu principal

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MenuPrincipal : MonoBehaviour
 {
     public GameObject Configuracoes;
+    public GameObject Principal;
 
     void Start()
     {
@@ -12,12 +13,15 @@ public class MenuPrincipal : MonoBehaviour
 
     void Update()
     {
-
+               if (Input.GetKeyDown(KeyCode.Escape)) {
+			AtivarMenuConfiguracoes();
+		}
     }
 
     public void AtivarMenuConfiguracoes()
     {
-        Configuracoes.SetActive(true);
-        this.gameObject.SetActive(false);
+        Configuracoes.SetActive(false);
+        Principal.SetActive(true);
+// Caso tenha outros menus coloca-los como desativados deixando somente o menu principal
     }
 }
